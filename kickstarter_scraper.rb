782127c02abc272b959295d51d44ab3a9703dd00
Hanna Lee <mani21na@gmail.com>
2019-09-09 20:37:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   # projects: kickstarter.css("li.project.grid_4")
-  # title: project.css()
+  # title: project.css("h2.bbcard_name string a").text
   binding.pry
 end
 
